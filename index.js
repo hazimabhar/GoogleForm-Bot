b4c9dfb2c411fa80197242f8e2b5b1185477eaac
hazimabhar <hazimabhar123@gmail.com>
2023-11-17 11:07:12 +0800

    siappppppp

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const puppeteer = require('puppeteer')
 
-let page;
-
 const getRandomName = require('./name/randomName')
 const getUserEmail = require('./email/emailUser')
 
@@ -18,13 +16,11 @@ const getAnswer10 = require('./question/tenthQuestion')
 const getAnswer11 = require('./question/eleventhQuestion')
 const getAnswer12 = require('./question/twelfthQuestion')
 
-
 async function launchPuppeteer(){
     const browser = await puppeteer.launch({headless: false});
-    page = await browser.newPage();
+    const page = await browser.newPage();
     //change the url according to the form you want to submit
     await page.goto('https://forms.gle/mXwTvt7zRJAebHCUA')
-    await page.waitForNavigation(); // Wait for the page to load
 
     let name = getRandomName()
 
@@ -34,84 +30,39 @@ async function launchPuppeteer(){
     //Question Type : Short Answer
     await page.type('input[aria-labelledby="i5"]', getUserEmail(name))
 
-    // await page.click('div[class="uArJ5e UQuaGc YhQJj zo8FOc ctEux"]')
-
-    // await page.click(`div[aria-label="${getAnswer1()}"]`);
-
-    // await page.click(`div[aria-label="${getAnswer2()}"]`);
-
-    // await page.click(`div[aria-label="${getAnswer3()}"]`);
-
-    // await page.click(`div[aria-label="${getAnswer4()}"]`);
-
-
-      // Function to choose the rate for a question
-    // const chooseRating = async (questionId, ratingValue) => {
-    //   await page.evaluate((id, value) => {
-    //     const radioElement = document.querySelector(`div[jsname="cnAzRb"] input[data-value="${value}"]`);
-    //     if (radioElement) {
-    //       radioElement.click();
-    //     }
-    //   }, questionId, ratingValue);
-    // };
-  
-    // // Choose rates for both questions
-    // await chooseRating('i1', '3'); // Choose rate 3 for Question 1
-    // await chooseRating('i5', '4'); // Choose rate 4 for Question 2
-
-    // await page.click('div[aria-labelledby="i1"] [aria-posinset="1"] ');
-
-    // await page.click('div[aria-labelledby="i5"] [aria-posinset="5"] ');
-
     //Question Type : Linear Scale
-    linearScale("i9", getAnswer1())
-    linearScale("i13", getAnswer2())
+    await linearScale(page, "i9", getAnswer1());
+    await linearScale(page, "i13", getAnswer2());
 
     //Question Type : Multiple Choice Grid
-    // const answers = [getAnswer3(), getAnswer4(), getAnswer5(), getAnswer6()];
+    const multipleChoiceGridAnswers = [getAnswer3(), getAnswer4(), getAnswer5(), getAnswer6(), getAnswer7(), getAnswer8(), getAnswer9(), getAnswer10()];
+    for (const answer of multipleChoiceGridAnswers) {
+      await page.click(`div[aria-label="${answer}"]`)
+    }
 
-    // for (const answer of answers) {
-    //   await page.click(`div[aria-label="${answer}"]`);
-    // }
+    //Question Type : Multiple Choice 
+    const multipleChoiceAnswers =[ getAnswer11(), getAnswer12()]
+    for (const answer of multipleChoiceAnswers) {
+      await page.click(`div[id="${answer}"]`)
+    }
 
-    // multipleChoice(getAnswer11())
-
-    // multipleChoice(getAnswer12())
-
-
-        //Question Type : Paragraph
-        await page.type('textarea[aria-labelledby="i63"]', 'No')  
-
-    // await page.click('.Od2TWd[aria-posinset="5"]');
-
-    // await page.click(`div[aria-label=test] div[data-value=${getAnswer1()}]`);
-
-    // await page.click(`div[aria-label=row2] div[data-value=${getAnswer2()}]`);
-
-    // await page.click(`div[aria-label=row3] div[data-value=${getAnswer3()}]`);
-
-    // await page.click(`div[aria-label=row4] div[data-value=${getAnswer4()}]`);
+    //Question Type : Paragraph
+    await page.type('textarea[aria-labelledby="i63"]', 'No')  
 
     await page.click('div[class="uArJ5e UQuaGc Y5sE8d VkkpIf QvWxOd"]');
 
     await browser.close();
 }
 
-async function linearScale(questionId, scale) {
-
-  await page.click(`div[aria-labelledby="${questionId}"] [aria-posinset="${scale}"]`);
-
-}
-
-async function multipleChoice(answer){
-  await page.click(`div[id=${answer}]`);
+async function linearScale(page, questionId, getAnswer) {
+  await page.click(`div[aria-labelledby="${questionId}"] [aria-posinset="${getAnswer}"]`);
 }
 
 function recursion(){
   process.setMaxListeners(20);
   console.time('HazimBot'); 
-  for(let i =0; i< 10; i++){
-    launchPuppeteer()
+  for(let i =0; i< 15; i++){
+     launchPuppeteer()
   }
   console.timeEnd('HazimBot');
   console.log('Memory Usage:', process.memoryUsage());
